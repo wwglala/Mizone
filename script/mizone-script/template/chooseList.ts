33c9697dbf696253f5f087caa5f1571d24c3f9f2
lalala <912228747@qq.com>
2022-10-13 22:39:45 +0800

    style: 整理

---
+++ script/mizone-script/template/chooseList.ts
@@ -0,0 +1,25 @@
+export const questionList = [
+  {
+    type: "list",
+    name: "operator",
+    message: "请选择你的操作",
+    choices: [
+      {
+        value: 1,
+        name: "创建组件",
+      },
+      {
+        value: 2,
+        name: "生成changelog",
+      },
+      {
+        value: 3,
+        name: "升级alpha",
+      },
+      {
+        value: 4,
+        name: "升级bata",
+      },
+    ],
+  },
+];
