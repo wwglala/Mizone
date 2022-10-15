5eedf00035983ea7d797e7681f60edc09c943b0d
lalala <912228747@qq.com>
2022-10-15 23:46:02 +0800

    fix: animation

--- packages/components/Animate/index.tsx
+++ packages/components/Animate/index.tsx
@@ -6,33 +6,43 @@ import React, {
   ReactElement,
 } from "react";
 interface AnimateProps extends HTMLAttributes<HTMLDivElement> {
-  duration: number;
+  appear: string;
+  exit: string;
   visible: boolean;
-  showClass: string;
-  hideClass: string;
+  duration: number;
   children?: ReactElement;
 }
-export const Animate = (props: AnimateProps) => {
-  const { visible, duration, children, showClass, hideClass } = props;
-  const [show, setShow] = useState(visible);
+
+function useDelayUnmount(visible, duration) {
+  const [desctoryed, setDesctory] = useState(!visible);
 
   useEffect(() => {
     if (!visible) {
       setTimeout(() => {
-        setShow(false);
+        setDesctory(true);
       }, duration);
-    } else {
-      setShow(true);
     }
   }, [visible]);
 
-  return show
-    ? React.cloneElement(children!, {
-        className: cx(
-          children?.props.className,
-          visible ? showClass : hideClass
-        ),
-      })
-    : null;
+  return desctoryed;
+}
+
+export const Animate = (props: AnimateProps) => {
+  const { appear, exit, visible, duration, children } = props;
+  const desctoryed = useDelayUnmount(visible, duration);
+
+  return (
+    <>
+      {desctoryed
+        ? null
+        : React.cloneElement(children!, {
+            className: cx(
+              children?.props.className,
+              { [appear]: visible },
+              { [exit]: !visible }
+            ),
+          })}
+    </>
+  );
 };
 Animate.displayName = "Animate";
