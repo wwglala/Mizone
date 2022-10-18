d5f9efdacddb4a6d81551764d63c1a1a2e3fc74c
wwglalala <912228747@qq.com>
2022-10-18 23:32:05 +0800

    feat: tooltip

--- packages/components/Tooltip/index.tsx
+++ packages/components/Tooltip/index.tsx
@@ -1,15 +1,27 @@
-
+import { cx } from "@mizone/utils";
 import React, { HTMLAttributes, ReactNode, forwardRef } from "react";
+import { useBem } from "../utils";
 
 interface TooltipProps extends HTMLAttributes<HTMLDivElement> {
   children?: ReactNode;
+  content: string;
+  position: "left" | "top" | "right" | "bottom";
 }
 export const Tooltip = forwardRef<HTMLDivElement, TooltipProps>(
   (props, forwardedRef) => {
-    const { children } = props;
+    const { children, content, position = "top" } = props;
 
-    return <div ref={forwardedRef}>Tooltip</div>;
+    const bem = useBem();
+    return (
+      <div
+        ref={forwardedRef}
+        // @ts-ignore
+        content={content}
+        className={cx(bem("tooltip"), bem("tooltip", { [position]: true }))}
+      >
+        {children}
+      </div>
+    );
   }
 );
 Tooltip.displayName = "Tooltip";
-  
--- packages/components/Tooltip/stories/Tooltip.stories.tsx
+++ packages/components/Tooltip/stories/Tooltip.stories.tsx
@@ -1,5 +1,4 @@
-
-import React from "react";
+import React, { CSSProperties } from "react";
 import { Tooltip } from "../index";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import "../style/index.scss";
@@ -10,15 +9,49 @@ export default {
   Component: Tooltip,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   // https://storybook.js.org/docs/react/essentials/controls#choosing-the-control-type
-  argTypes:{
-    
-  }
+  argTypes: {},
 } as ComponentMeta<typeof Tooltip>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-export const Template: ComponentStory<typeof Tooltip> = (args) => <Tooltip {...args} />;
+export const Template: ComponentStory<typeof Tooltip> = (args) => {
+  const style: CSSProperties = {
+    width: 100,
+    margin: "20px",
+    background: "skyblue",
+    borderRadius: "5px",
+    margin: "10px",
+  };
+
+  return (
+    <div
+      style={{
+        position: "absolute",
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        margin: "auto",
+      }}
+    >
+      <Tooltip {...args} position="top" content="top">
+        <div style={style}>top</div>
+      </Tooltip>
+      <Tooltip {...args} position="left" content="left">
+        <div style={style}>left</div>
+      </Tooltip>
+      <Tooltip {...args} position="right" content="right">
+        <div style={style}>right</div>
+      </Tooltip>
+      <Tooltip {...args} position="bottom" content="bottom">
+        <div style={style}>bottom</div>
+      </Tooltip>
+    </div>
+  );
+};
 
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Template.args = {
-
+  children: "123123",
+  content: "xxxxx",
+  position: "bottom",
 };
